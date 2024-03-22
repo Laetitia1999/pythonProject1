6197a11bd69bfa958a72de999a4ea20b4ad320f3
laeti <laetitia_sfeir@hotmail.com>
2024-03-22 19:14:53 +0100

    1

---
+++ main.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+import pandas as pd
+
+import repository
+import view
+import toml
+
+
+#def main():
+# Chargement de la configuration depuis config.toml
+config = toml.load('config.toml')
+# Obtenez le chemin à partir de la configuration
+input_path = config["input"]["input_path"]
+bq_sheet = config['input']['sheet_bank']
+erp_sheet = config['input']['sheet_server']
+tableau_sheet = config['input']['sheet_data']
+column_status = config['input']['column_status']
+column_amount = config['input']['column_amount']
+
+
+
+# get data from excel_file
+df_bq = pd.read_excel(input_path, sheet_name=config["input"]["sheet_bank"])
+df_erp = pd.read_excel(input_path, sheet_name=config["input"]["sheet_server"])
+excel = pd.read_excel(input_path, sheet_name=config["input"]["sheet_data"])
+excel['Date'] = excel['Date'].apply(lambda x: datetime.strftime(x, "%B"))
+
+#column_status = 'Status'
+#column_amount = 'Montant1'
+
+# format and merge dataframes
+df_bq = repository.reformat_file(df_bq)
+df_erp = repository.reformat_file(df_erp)
+inner_join = pd.merge(df_erp, df_bq, on='Numéro', how='inner')
+repository.get_status(df=inner_join, column_status=column_status)
+
+print()
+
+# Open excel workbook and print the status in sheet BQ
+view.print_data(input_path=input_path, df=inner_join, column_status=column_status)
+
+# get invoices and amounts for which a reminder e-mail must be sent
+final_list = repository.get_list_reminder(df_erp)
+
+# sent reminder email
+view.mail(config['mail']['smtp_server'], config['mail']['sender_email'], config['mail']['sender_password'],
+          config['mail']['recipient_email'], config['mail']['mail_subject'], config['mail']['mail_body'],
+          final_list)
+
+# creating the necessary dataframe for graphic and table
+table1 = repository.create_tables(excel, column_amount, column_status)[0]
+table2 = repository.create_tables(excel, column_amount, column_status)[1]
+
+# generate chart and pdf
+view.graph(config, table1, table2)
+
+# graphique
+for graph_config in config['graphics']:
+    if graph_config['id'] == 1:
+        view.graph(table1, graph_config['options'])
+    elif graph_config['id'] == 2:
+        view.graph(table2, graph_config['options'])
+"""
---
+++ repository.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+import toml
+import numpy as np
+import pandas as pd
+
+
+def reformat_file(dataframe, config):
+    """
+    add currency column, Montant column and drop columns Montant USD and EUR
+    return a reformated data frame
+    :param dataframe: DataFrame
+    :return: reformated dataFrame
+    """
+    dataframe['CCY'] = dataframe.apply(lambda x: 'USD' if np.isnan(x[config['input']['column_amount']]) else 'EUR', axis=1)
+    dataframe['Montant'] = dataframe.apply(lambda x: x[config['input']['column_amount']]
+     if x['CCY'] == 'EUR' else x[config['input']['column_amount']], axis=1)
+    dataframe = dataframe.drop(columns=[config['input']['column_amount'] + ' EUR', config['input']['column_amount'] + ' USD'])
+    return dataframe
+def main () :
+    config = toml.load('config.toml')
+
+def get_status(df, config):
+    """ give the status for each receipt
+    :param df: DataFrame
+    :return: fill column Status
+    """
+    column_status = config['input']['column_status']
+    for i in range(len(df['Numéro'])):
+        if df['Montant_x'][i] == df['Montant_y'][i]:
+            df[column_status][i] = "Payed"
+        elif df['Montant_x'][i] <= df['Montant_y'][i]:
+            df[column_status][i] = "Payed with change gain"
+        else:
+            df[column_status][i] = "Payed with losses"
+
+
+def create_tables(df, config):
+    """
+    Create the tables necessary for the pdf
+    :param df: Dataframe necessary for the table
+    :param column_amount: amount
+    :param column_status: status
+    :return: list of tables
+    """
+    column_amount = config['input']['column_amount']
+    column_status = config['input']['column_status']
+    table = pd.pivot_table(df, values=column_amount, index=column_status,
+                           aggfunc={column_amount: ["count", np.sum, np.mean, np.std, np.median]})
+
+    table.loc['Total'] = [len(df[column_status]), df[column_amount].mean(), df[column_amount].median(),
+                          df[column_amount].std(), df[column_amount].sum()]
+    table = table.round(decimals=2)
+    table = table.reset_index(drop=False)
+
+    tab = pd.pivot_table(df, values=column_amount, index='Date', columns=column_status, aggfunc="sum", fill_value=0,
+                         margins=True, margins_name="Total")
+    tab = tab.drop('Total', axis=0).reset_index().round(2)
+
+    return [table, tab]
+
+
+def get_list_reminder(df, config):
+    """
+    Get the list of info for each client that need to be reminded of their none payment
+    :param df: Dataframe
+    :return: list of tuple with client info
+    """
+    today = datetime.today()
+    reminder_threshold = config['reminder']['threshold']
+    column_invoice_number = config['input']['column_invoice_number']
+    column_amount = config['input']['column_amount']
+
+    df['reminder'] = (datetime.today() - df["Date de facture"]).dt.days
+    df = df[df['reminder'] >= 30]
+    facture = df['Numéro'].to_list()
+    amont = df['Montant'].to_list()
+    final_list = list(zip(facture, amont))
+    return final_list
